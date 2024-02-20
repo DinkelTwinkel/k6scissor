2d9a1f630d8a02ac1a9ff3275dfae6182bd1ed28
DinkelTwinkel <128545374+DinkelTwinkel@users.noreply.github.com>
2024-02-20 06:44:22 +0000

    push

--- patterns/cutOffClock.js
+++ patterns/cutOffClock.js
@@ -16,7 +16,7 @@ module.exports = async (client) => {
     nextUTCDay.setSeconds(0);
     nextUTCDay.setDate(result.nextDate);
 
-    const differenceMiliUTC = nextUTCDay.getTime() - currentDate.getTime();
+    const differenceMiliUTC = result.nextDate - currentDate.getTime();
 
     console.log (differenceMiliUTC);
     console.log (currentDate.getTime());
