ffce917c1d0c768c644c10f8de7200d906f00de1
DinkelTwinkel <128545374+DinkelTwinkel@users.noreply.github.com>
2024-03-01 15:41:57 +0000

    Update postListener.js

--- patterns/postListener.js
+++ patterns/postListener.js
@@ -195,7 +195,7 @@ module.exports = async (client) => {
     });
 
     function attachmentTest(message) {
-        const imageExtensions = /\.(png|jpeg|jpg|jpg|webp|gif)/i;
+        const imageExtensions = /\.(png|jpeg|jpg|jpg|webp|gif|mp4)/i;
     
         if (message.attachments.size > 0) {
             const attachment = message.attachments.first(); // Get the first attachment (usually the most recent one)
